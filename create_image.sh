db2b69c7b8c14ea0cf2dc09bc68c997defd8c16d
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-06-06 14:49:09 +0530

    Update create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -4,6 +4,6 @@ declare -r IMAGE_NAME="ajay8142917337/java-docker-build-tutorial"
 declare -r IMAGE_TAG="1.0"
 
 echo "Building image '$IMAGE_NAME:$IMAGE_TAG'"
-docker build -t javaDocker:1.0 .
-docker tag javaDocker:1.0 ajay8142917337/docker-test:1.0
+docker build -t javadocker:1.0 .
+docker tag javadocker:1.0 ajay8142917337/docker-test:1.0
 docker push ajay8142917337/docker-test:1.0
