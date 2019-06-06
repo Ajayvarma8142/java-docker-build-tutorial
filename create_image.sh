ab8f59bdd05c08e2dc01917bf091a3e9b35d0604
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-06-06 12:39:03 +0530

    Update create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -6,4 +6,4 @@ declare -r IMAGE_TAG="latest"
 echo "Building image '$IMAGE_NAME:$IMAGE_TAG'"
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
 docker tag $IMAGE_NAME:$IMAGE_TAG ajay8142917337/docker-test:$IMAGE_TAG
-docker push ajay8142917337/docker-test:$IMAGE_TAG
+docker push ajay8142917337/docker-test:$IMAGE_TAG\
